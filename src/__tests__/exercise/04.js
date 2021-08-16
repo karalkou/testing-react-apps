8900e31969fc3dc4b2877d5049efe1406de2d3cd
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-16 21:34:54 +0300

    No-ticket: Finish 'Generate Test Data' extra task
    Use 'faker' to generate username and password for our Login

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,14 +5,16 @@ import * as React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+const buildLoginForm = () => ({ username: faker.internet.userName(), password: faker.internet.password() })
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const usernameTest = 'chucknorris'
-  const passwordTest = 'i need no password'
+  const { username: usernameTest, password: passwordTest } = buildLoginForm()
 
   const userNameField = screen.getByLabelText(/username/i)
   const passwordField = screen.getByLabelText(/password/i)
