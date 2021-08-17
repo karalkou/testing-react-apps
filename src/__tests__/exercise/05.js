03f018adabd23c42590e00f080108d1c821a2cda
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-17 17:04:28 +0300

    No-ticket: Finish 'Use One-off Server Handlers' extra task
    Added a test case for 'unknown server error'. To override server response we used server.use(). And to avoid troubles with overrided server response we added 'server.resetHandlers' after each test case run

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { build, fake } from '@jackfranklin/test-data-bot'
+import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { handlers } from 'test/server-handlers'
 import Login from '../../components/login-submission'
@@ -20,6 +21,7 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -47,4 +49,22 @@ test('not sending password to server', async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test('unknown server error displays the error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: testErrorMessage }))
+      },
+    ),
+  )
+  render(<Login />)
+  userEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
