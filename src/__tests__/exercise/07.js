dddc067d9d59b3eb145d30f4c8c8dd4e1107c9ed
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-19 22:29:50 +0300

    No-ticket: Finish 'Render Method' extra task
    created a custom render function with Wrapper inside

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,16 +2,19 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render as rtlRender, screen } from '@testing-library/react'
 import { ThemeProvider } from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-function Wrapper(theme) {
-  return ({ children }) => <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+function render(ui, { theme = 'light', ...options } = {}) {
+  const Wrapper = ({ children }) => (
+    <ThemeProvider value={[theme, () => {}]}>{children}</ThemeProvider>
+  )
+  return rtlRender(ui, { wrapper: Wrapper, ...options })
 }
 
 test('renders with the light styles for the light theme', () => {
-  render(<EasyButton />, { wrapper: Wrapper('light') })
+  render(<EasyButton />)
 
   const button = screen.getByRole('button', { contents: /easy/i })
 
@@ -22,7 +25,7 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  const { rerender } = render(<EasyButton />, { wrapper: Wrapper('dark') });
+  render(<EasyButton />, { theme: 'dark' })
 
   const button = screen.getByRole('button', { contents: /easy/i })
 
