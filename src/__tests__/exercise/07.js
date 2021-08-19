e75e48b91aef707ee82d49519ae49e871e7672cc
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-19 22:09:36 +0300

    No-ticket: Finish 'Dark Theme' extra task
    added a test-case for dark theme

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,18 +6,29 @@ import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-function Wrapper({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+function Wrapper(theme) {
+  return ({ children }) => <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
 }
 
 test('renders with the light styles for the light theme', () => {
-  render(<EasyButton />, { wrapper: Wrapper })
+  render(<EasyButton />, { wrapper: Wrapper('light') })
 
   const button = screen.getByRole('button', { contents: /easy/i })
 
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const { rerender } = render(<EasyButton />, { wrapper: Wrapper('dark') });
+
+  const button = screen.getByRole('button', { contents: /easy/i })
+
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
 
