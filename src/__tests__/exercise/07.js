4f1470c7692116418b730537373ce1f0df3e9c52
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-19 22:01:48 +0300

    No-ticket: Finish 'Wrapper Component' main task
    we provided EasyButton component with ThemeProvider in our test, to have possibility to render EasyButton (it needs data from context)

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,21 +2,23 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+function Wrapper({ children }) {
+  return <ThemeProvider>{children}</ThemeProvider>
+}
+
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  render(<EasyButton />, { wrapper: Wrapper })
+
+  const button = screen.getByRole('button', { contents: /easy/i })
+
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
